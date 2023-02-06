1752a2d7e1a3a8f2fa3617fb920dd1886adbc721
Jakob Ankarhem <jakob.ankarhem@outlook.com>
2023-02-06 11:18:59 +0100

    init release

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# norce-upsell
+
+## 0.0.7
+
+### Patch Changes
+
+- initial release
--- src/App.svelte
+++ src/App.svelte
@@ -5,10 +5,9 @@
   import { addToCart, getProductsFromCatogory } from './jetshop';
   import { culture, t } from './translations';
 
-  // export let api: MountProps['api'];
-  export let api: any;
+  export let api: MountProps['api'];
   export let data: MountProps['data'];
-  export let EventEmitter: MountProps['EventEmitter'];
+  // export let EventEmitter: MountProps['EventEmitter'];
 
   const formatter = createFormatter(data.order.culture, data.order.currency);
 
@@ -27,6 +26,9 @@
     culture.set(data.order.culture);
   });
 
+  /**
+   * This is a minimal example and does not work for variants etc.
+   **/
   const handleClick = (articleNumber: string) => {
     addToCart({
       cartId: data.order.cart?.reference,
@@ -46,34 +48,36 @@
     </h2>
     <ul class="flex gap-4">
       {#each products as product}
-        <li
-          class="grid gap-2"
-          data-testid={`upsell-item-${product?.articleNumber}`}
-        >
-          <img
-            src={product?.images?.[0]?.url}
-            alt={product?.images?.[0]?.alt}
-            class="w-48 h-48 object-contain"
-          />
-          <h3>{product?.name}</h3>
-          <div class="flex justify-between">
-            <span class="text-xs">
-              {formatter.format(product?.price?.incVat)}
-            </span>
-            {#if product?.previousPrice?.incVat}
-              <span class="text-xs line-through text-base-content/50">
-                {formatter.format(product?.previousPrice?.incVat)}
-              </span>
-            {/if}
-          </div>
-          <button
-            data-testid="norce-upsell-add-to-cart"
-            class="bg-primary text-primary-content rounded py-2"
-            on:click={() => handleClick(product?.articleNumber)}
+        {#if !product?.hasVariants}
+          <li
+            class="grid gap-2"
+            data-testid={`upsell-item-${product?.articleNumber}`}
           >
-            {t('Add to cart')}
-          </button>
-        </li>
+            <img
+              src={product?.images?.[0]?.url}
+              alt={product?.images?.[0]?.alt}
+              class="w-48 h-48 object-contain"
+            />
+            <h3>{product?.name}</h3>
+            <div class="flex justify-between">
+              <span class="text-xs">
+                {formatter.format(product?.price?.incVat)}
+              </span>
+              {#if product?.previousPrice?.incVat}
+                <span class="text-xs line-through text-base-content/50">
+                  {formatter.format(product?.previousPrice?.incVat)}
+                </span>
+              {/if}
+            </div>
+            <button
+              data-testid="norce-upsell-add-to-cart"
+              class="bg-primary text-primary-content rounded py-2"
+              on:click={() => handleClick(product?.articleNumber)}
+            >
+              {t('Add to cart')}
+            </button>
+          </li>
+        {/if}
       {/each}
     </ul>
   </div>
